3841102938dbf363919eb172e34104132fb161e4
Ystas24 <yroslavromanovskij@gmail.com>
2024-02-28 23:14:50 +0200

    da

---
+++ levels.py
@@ -0,0 +1,22 @@
+LEVEL_1 = {
+    "1": [50, 650, 200, 12, "images/walls/back_wall.jpg"],
+    "2": [35, 442, 15, 220, "images/walls/left_wall.jpg"],
+    "3": [50, 442, 200, 55, "images/walls/front_wall.jpg"],
+    "4": [250, 442, 15, 85, "images/walls/half1-right_wall.jpg"],
+    "5": [250, 578, 15, 85, "images/walls/half2-right_wall.jpg"],
+    "6": [264, 482, 160, 45, "images/walls/front_wall.jpg"],
+    "7": [264, 578, 160, 12, "images/walls/back_wall.jpg"],
+    "8": [540, 321, 15, 100, "images/walls/half1-left_wall.jpg"],
+    "9": [635, 321, 15, 100, "images/walls/half1-right_wall.jpg"],
+    "10": [420, 418, 15, 110, "images/walls/half1-left_wall.jpg"],
+    "11": [420, 577, 15, 110, "images/walls/half2-left_wall.jpg"],
+    "12": [435, 419, 120, 55, "images/walls/left-front_wall.jpg"],
+    "13": [635, 419, 120, 55, "images/walls/right-front_wall.jpg"],
+    "14": [755, 419, 15, 267, "images/walls/right_wall.jpg"],
+    "15": [435, 674, 320, 12, "images/walls/back_wall.jpg"]
+}
+
+
+LEVEL_2 = {
+
+}
